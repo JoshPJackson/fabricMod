cc76eea717f6f256c68af23419eaeb24ea69d3f7
Joshua Jackson <joshpjackson12@gmail.com>
2019-12-24 14:18:15 +0000

    updated

--- src/main/java/net/fabricmc/examplemod/ExampleModClientInitializer.java
+++ src/main/java/net/fabricmc/examplemod/ExampleModClientInitializer.java
@@ -1,6 +1,7 @@
 package net.fabricmc.examplemod;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.examplemod.blocks.BaseFluidBlock;
 import net.fabricmc.examplemod.client.FluidResourceLoader;
 import net.fabricmc.examplemod.register.FluidRegistry;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
@@ -11,12 +12,14 @@ import net.minecraft.client.texture.SpriteAtlasTexture;
 import net.minecraft.resource.ResourceType;
 import net.minecraft.util.Identifier;
 
+
 public class ExampleModClientInitializer implements ClientModInitializer {
 	
 	@Override
 	public void onInitializeClient() {
 		ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(new FluidResourceLoader());
 		
+		BlockRenderLayerMap.INSTANCE.putBlock(new BaseFluidBlock(FluidRegistry.STILL_ACID), RenderLayer.getTranslucent());
 		BlockRenderLayerMap.INSTANCE.putFluid(FluidRegistry.STILL_ACID, RenderLayer.getTranslucent());
 		BlockRenderLayerMap.INSTANCE.putFluid(FluidRegistry.FLOWING_ACID, RenderLayer.getTranslucent());
 		
